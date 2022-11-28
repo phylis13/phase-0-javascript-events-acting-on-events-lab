eb6a86cab210c66aa922f4f92a87c64028a51896
phylis13 <phylisnjeri54@gmail.com>
2022-11-28 22:14:48 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Your code here
+function moveDodgerLeft(){
+    let dodger = document.getElementById("dodger");
+    let left = parseInt(dodger.style.left);
+    left -= 20;
+    dodger.style.left = left+"px";
+ }
+ 
+ function moveDodgerRight(){
+     let dodger = document.getElementById("dodger");
+     let left = parseInt(dodger.style.left);
+     left += 20;
+     dodger.style.left = left+"px";
+ }
+ 
+ console.log(moveDodgerLeft())
